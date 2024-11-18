d8db96328e15835066ce6e7fb9cb228a070e253e
Felipe Cordeiro <felipecordeirocruz@gmail.com>
2024-11-18 17:25:53 -0300

    Pagination

--- backend/src/main/java/com/felipe/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/felipe/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.felipe.dscatalog.resources.exceptions;
 
+import com.felipe.dscatalog.services.exceptions.DatabaseException;
 import com.felipe.dscatalog.services.exceptions.ResouceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -14,13 +15,25 @@ public class ResourceExceptionHandler {
 
     @ExceptionHandler(ResouceNotFoundException.class)
     public ResponseEntity<StandardError> entityNotFound(ResouceNotFoundException e, HttpServletRequest request) {
-
+        HttpStatus status = HttpStatus.NOT_FOUND;
         StandardError err = new StandardError();
         err.setTimestamp(Instant.now());
-        err.setStatus(HttpStatus.NOT_FOUND.value());
+        err.setStatus(status.value());
         err.setError("Resource not found");
         err.setMessage(e.getMessage());
         err.setPath(request.getRequestURI());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Database exception");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
     }
 }
--- backend/src/main/java/com/felipe/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/felipe/dscatalog/services/CategoryService.java
@@ -3,13 +3,17 @@ package com.felipe.dscatalog.services;
 import com.felipe.dscatalog.dto.CategoryDTO;
 import com.felipe.dscatalog.entities.Category;
 import com.felipe.dscatalog.repositories.CategoryRepository;
+import com.felipe.dscatalog.services.exceptions.DatabaseException;
 import com.felipe.dscatalog.services.exceptions.ResouceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,9 +23,9 @@ public class CategoryService {
     private CategoryRepository repository;
 
     @Transactional(readOnly = true)
-    public List<CategoryDTO> findAll() {
-        List<Category> list = repository.findAll();
-        return list.stream().map(CategoryDTO::new).toList();
+    public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+        Page<Category> list = repository.findAll(pageRequest);
+        return list.map(CategoryDTO::new);
     }
 
     @Transactional(readOnly = true)
@@ -49,6 +53,18 @@ public class CategoryService {
        } catch (EntityNotFoundException e) {
             throw new ResouceNotFoundException("Id not found " + id);
        }
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new ResouceNotFoundException("Id not found " + id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
+        }
     }
 }
 
