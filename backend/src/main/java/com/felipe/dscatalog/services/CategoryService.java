9ea2806ee8b7b47c552830d8e5435147b5ae66d8
Felipe Cordeiro <felipecordeirocruz@gmail.com>
2024-11-17 22:18:35 -0300

    H2 database

---
+++ backend/src/main/java/com/felipe/dscatalog/services/CategoryService.java
@@ -0,0 +1,20 @@
+package com.felipe.dscatalog.services;
+
+import com.felipe.dscatalog.entities.Category;
+import com.felipe.dscatalog.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+}
+
