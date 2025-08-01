ada4b9f5ce1cb212f9434fd6d5eb3fd2df6cd973
Felipe Cordeiro <felipecordeirocruz@gmail.com>
2025-07-31 23:06:00 -0300

    feat: add JwtAuthenticationFilter to SecurityConfig for enhanced authentication

--- backend/src/main/java/com/felipecordeiro/dscatalog/config/SecurityConfig.java
+++ backend/src/main/java/com/felipecordeiro/dscatalog/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.felipecordeiro.dscatalog.config;
 
+import com.felipecordeiro.dscatalog.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -31,6 +35,7 @@ public class SecurityConfig {
                 .headers(headers -> headers
                         .frameOptions(frameOptions -> frameOptions.disable())
                 )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
